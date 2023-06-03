29086580ca074584bc5b5ebe39625db9463eee29
purquiza <purquiza@ucb.edu.bo>
2023-06-03 18:51:37 -0400

    Primer commit

---
+++ Modulo-01/index.js
@@ -0,0 +1,13 @@
+console.log("Gabriela"+" "+"Salgueiro"+" "+"(1978)");
+document.getElementById("imagen").src="https://rickandmortyapi.com/api/character/avatar/361.jpeg";
+function cambiarimagen1 () {
+    document.getElementById("imagen").src="https://rickandmortyapi.com/api/character/avatar/361.jpeg";
+}
+
+function cambiarimagen2 () {
+    document.getElementById("imagen").src="https://rickandmortyapi.com/api/character/avatar/1.jpeg";
+}
+
+
+
+
